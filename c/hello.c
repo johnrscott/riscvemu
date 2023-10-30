6ed1fc65c6c59bc16ddbde2ebab1cbe1a033fcbd
John Scott <johnrscott0@gmail.com>
2023-10-30 19:13:18 +0000

    Got a simple version of linking working

--- c/hello.c
+++ c/hello.c
@@ -1,10 +1,11 @@
-
+// This emits a warning currently, might be a
+// compiler bug:
+// https://stackoverflow.com/questions/71383351/
+// how-to-avoid-wrong-array-bounds-warning-on-a-pointer-in-g12
 static volatile char *dev = (char*)0x3f8;
 
 int main() {
-    *dev = 'h';
+    *dev = '0';
     while (1)
 	;
 }
-
-
