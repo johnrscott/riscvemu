f03103b3bac715b97dc0e2c1cabb0c093ca0d78b
John Scott <johnrscott0@gmail.com>
2023-10-30 23:09:47 +0000

    Checked stack manipulation works

--- c/hello.c
+++ c/hello.c
@@ -1,11 +1,24 @@
-// This emits a warning currently, might be a
-// compiler bug:
-// https://stackoverflow.com/questions/71383351/
-// how-to-avoid-wrong-array-bounds-warning-on-a-pointer-in-g12
-static volatile char *dev = (char*)0x3f8;
+
+void putchar(char ch) {
+    // This emits a warning currently, might be a
+    // compiler bug:
+    // https://stackoverflow.com/questions/71383351/
+    // how-to-avoid-wrong-array-bounds-warning-on-a-pointer-in-g12
+    static volatile char *dev = (char*)0x3f8;
+    *dev = ch;
+}
+
+int triangle_number(int n) {
+    if (n == 0) {
+	return 0;
+    } else {
+	return n + triangle_number(n-1);
+    }
+}
 
 int main() {
-    *dev = '5';
+    int m = triangle_number(3);
+    putchar(0x30 + m);
     while (1)
 	;
 }
