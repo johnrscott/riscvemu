0e0aaf120e762def5c22f1ad570ea9b70b67e65c
John Scott <johnrscott0@gmail.com>
2023-12-09 19:36:41 +0000

    Fixed ecall handling

--- toolchains/newlib/csr.h
+++ toolchains/newlib/csr.h
@@ -1,6 +1,8 @@
 // Control/status register (CSR) addresses
 #define CSR_MSTATUS 0x300
 #define CSR_MIE 0x304
+#define CSR_MEPC 0x341
+#define CSR_MCAUSE 0x342
 
 // CSR fields
 #define MSTATUS_MIE (1 << 3)
--- toolchains/newlib/trap.c
+++ toolchains/newlib/trap.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include "interrupts.h"
+#include "csr.h"
+
+#define MCAUSE_INSTRUCTION_ADDRESS_MISALIGNED 0
+#define MCAUSE_INSTRUCTION_ACCESS_FAULT 1
+#define MCAUSE_ILLEGAL_INSTRUCTION 2
+#define MCAUSE_BREAKPOINT 3
+#define MCAUSE_LOAD_ADDRESS_MISALIGNED 4
+#define MCAUSE_LOAD_ACCESS_FAULT 5
+#define MCAUSE_STORE_ADDRESS_MISALIGNED 6
+#define MCAUSE_STORE_ACCESS_FAULT 7
+#define MCAUSE_MMODE_ECALL 11
 
 void _nmi_handler() {
     printf("nmi");
@@ -7,10 +18,56 @@ void _nmi_handler() {
 	;
 }
 
+inline __attribute__((always_inline)) int read_csr(int csr_num) {
+    int result;
+    asm("csrr %0, %1"
+	: "=r"(result)
+	: "I"(csr_num));
+    return result;
+}
+
+inline __attribute__((always_inline)) void write_csr(int csr_num, int value) {
+    asm("csrw %0, %1"
+	: /* no outputs */
+	: "I"(csr_num), "r"(value));
+}
+
+
 void _exception_handler() {
-    printf("exception");
-    while (1)
-	;
+
+    // You cannot use stdout inside this function because the C
+    // environment is not necessarily set up yet.
+    
+    int mcause = read_csr(CSR_MCAUSE);
+
+    // Assume that mcause does not have the interrupt bit set
+    switch(mcause) {
+    case MCAUSE_INSTRUCTION_ADDRESS_MISALIGNED:
+	break;
+    case MCAUSE_INSTRUCTION_ACCESS_FAULT:
+	break;
+    case MCAUSE_ILLEGAL_INSTRUCTION:
+	break;
+    case MCAUSE_BREAKPOINT:
+	break;
+    case MCAUSE_LOAD_ADDRESS_MISALIGNED:
+	break;
+    case MCAUSE_LOAD_ACCESS_FAULT:
+	break;
+    case MCAUSE_STORE_ADDRESS_MISALIGNED:
+	break;
+    case MCAUSE_STORE_ACCESS_FAULT:
+	break;
+    case MCAUSE_MMODE_ECALL:
+	// The ecall is an nop in this platform
+	write_csr(CSR_MEPC, read_csr(CSR_MEPC) + 4);
+        break;
+    default:
+	// Unknown mcause
+	while(1)
+	    ;
+    }
+    asm("mret");
 }
 
 void _software_isr() {
