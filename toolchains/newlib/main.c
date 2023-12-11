0d848ef9e804896b009c72caf430c140d6b1a57e
John Scott <johnrscott0@gmail.com>
2023-12-11 18:02:34 +0000

    Still trying to debug the latest newlib issue

--- toolchains/newlib/main.c
+++ toolchains/newlib/main.c
@@ -1,21 +1,9 @@
 #include <stdio.h>
 
-/*
-print fahrenheit to celsius table for fahr = 0, 20, ..., 300;
-floating-point version. From page 12 of K&R.
-*/
 int main() {
-    float fahr, celsius;
-    int lower, upper, step;
 
-    lower = 0; /* lower limit of temperature table */
-    upper = 300; /* upper limit */
-    step = 20; /* step size */
-
-    fahr = lower;
-    while (fahr <= upper) {
-	celsius = (5.0/9.0) * (fahr - 32.0);
-	printf("%3.0f %6.1f\n", fahr, celsius);
-	fahr = fahr + step;
+    for (int i = 0; i < 20; i++) {
+	printf("The number is: %d", i);
     }
+
 }
